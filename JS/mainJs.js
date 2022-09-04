d2f1d1dcf53e3ce6db4b0eae5bd5517938bc0991
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-09-04 03:41:14 -0500

    fixed cockpit, finished moving cloud animation. decided on hover effect to raise the cockpit and show options on bigger screens/onclick open a dialogue box with table of contents on smaller devices

--- JS/mainJs.js
+++ JS/mainJs.js
@@ -7,21 +7,3 @@ let dayNight = $('#dayNight');
 dayNight.click(()=>{
     $(".Portrait").toggleClass("day night");
 });
-
-function onLoad(callBack){
-    let intervalId= window.setInterval(()=>{
-        if(document.getElementById('body')[0]!==undefined){
-            window.clearInterval(intervalId);
-            callBack.call(this);
-        }
-    },1000);
-}
-function setVisibility(target, visible){
-document.querySelector(target).style.display = visible ? 'block' : 'none';
-}
-
-onLoad(()=>{
-    setVisibility('Portrait',true);
-    // setVisibility('.loading',false);
-    }
-)
