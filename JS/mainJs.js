5258e949ec9693efe0f3305c10f559ea9d3d438b
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-09-05 01:40:29 -0500

    completed almost all art i desire on my page. last few are the contact us textbox, and text/textboxes for smaller devices

--- JS/mainJs.js
+++ JS/mainJs.js
@@ -1,9 +1,13 @@
 "use strict";
-let landSky = $('#landSky');
-landSky.click(()=>{
-  $(".Portrait").toggleClass("sky land");
+let landSky = $('#contactMe');
+landSky.click(() => {
+    $(".Portrait").toggleClass("sky land");
 });
 let dayNight = $('#dayNight');
-dayNight.click(()=>{
+dayNight.click(() => {
     $(".Portrait").toggleClass("day night");
+});
+
+$(".options").click(() => {
+    $(".options").toggleClass("active");
 });
