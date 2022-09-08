7d48708d6f98c0ab857ef1ccfd8bba4116424601
Manuel <manuel.dominguez1113@gmail.com>
2022-09-08 18:46:13 -0500

    almost complete. made a typo.. have to find it...

--- JS/mainJs.js
+++ JS/mainJs.js
@@ -3,23 +3,28 @@
 
 //loading screen
 window.addEventListener('load', () => {
+    $("#loadingScreen").removeClass("active");
     // works just need to make a quick loading page that loads first before everything else
 })
 
-//variables/constants
+//constants
 const landSky = $('#contactMe');
 const inverse = $("#home, #about, #skills, #work");
 const portrait = $(".Portrait");
 const buttons = $("#buttons");
 const dayNight = $('#dayNight');
 
-const textArea = $("#text")
+const textArea = $("#text");
+const textBox = $("#textBox");
 const home = $("#home");
 const about = $("#about");
 const skills = $("#skills");
 const work = $("#work");
 const contactMe = $("#contactMe");
-const windows= $("img.LargeTextBox, img.smallTextBox");
+const options = $(".options");
+const windows = $("img.LargeTextBox, img.smallTextBox");
+const toggleBackGround = $("#toggleBackground");
+const toggleTextBox = $("#toggleTextbox");
 
 landSky.click(() => {
     if (portrait.hasClass("sky")) {
@@ -39,9 +44,34 @@ dayNight.click(() => {
 
 
 //on touchscreen, will give active class to option the menu, doesn't matter in hover capable devices.
-$(".options").click(() => {
-    $(".options").toggleClass("active");
+options.click(() => {
+    options.toggleClass("active");
+    if(textBox.hasClass("toggle")) {
+        textArea.removeClass("toggle");
+        textBox.removeClass("toggle");
+        $("#OpenTextBox").addClass("toggle");
+        windows.toggleClass("on off");
+    }
 });
+
+//toggles
+toggleTextBox.click(() => {
+    windows.toggleClass("on off");
+    textArea.addClass("toggle");
+    setTimeout(()=>{
+        textBox.addClass("toggle");
+        $("#OpenTextBox").removeClass("toggle");
+    },150);
+
+})
+toggleBackGround.click(() => {
+    $("body").toggleClass("still animated");
+    if($("body").hasClass("still")){
+        toggleBackGround.html("Animated Stars?");
+    } else{
+        toggleBackGround.html("Stop Stars?");
+    }
+})
 
 
 //trying out a clock in the cockpit, which is not UI/UX friendly
@@ -67,7 +97,7 @@ const text = [
         name: 'Hello World!',
         field: "<div class=\"page page1 \"> <!--home-->\n" +
             "        <p class=\"line1\">I am Manuel Alex Dominguez</p>\n" +
-            "        <p class=\"line2\">Enthusiastic Web Developer</p>\n" +
+            "        <p class=\"line2\">Enthusiastic Web/Game Developer</p>\n" +
             "        <p class=\"line3\">Full-Stack Dev || Game Dev || Air Traffic Controller</p>\n" +
             "    </div>"
     },
@@ -75,22 +105,21 @@ const text = [
         id: 2,
         name: 'About Me',
         field: " <div class=\"page page2  \"> <!--about-->\n" +
-            "        <p class=\"line1\">I am an Air-force veteran, spending 10 years as an Air Traffic Controller, now an aspiring full stack developer\n" +
-            "        interested in creating for you, be it new and exciting websites, artistic and unique approaches, to working on\n" +
-            "        functionality and fluid\n" +
-            "        user interaction.</p>\n" +
+            "        <p class=\"line1\">I am an Air-force veteran, spending 10 years as an Air Traffic Controller, now following a dream of full stack developer\n" +
+            "        interested in creating for you! Be it new and exciting websites, artistic and unique approaches, to working on\n" +
+            "        backend functionality and fluid\n" +
+            "        user interaction!</p>\n" +
             "\n" +
-            "        <p class=\"line2\">I began in 2008 going through my first half of high school in Design and Technology Academy, but followed my\n" +
-            "        family's footsteps to serve my nation after graduation. Returning in late 2021-2022\n" +
-            "        to begin again in Code-up LLC. Immediately following Code-up with IGA to learn Indie gaming and C#</p>\n" +
-            "    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequatur dolorem eveniet, iusto neque nisi quod reiciendis sapiente temporibus unde voluptatem voluptates? Accusantium autem dignissimos eius ipsam quod ullam, velit!</div><div>Aliquid amet beatae culpa dolorem dolorum eius facilis ipsa itaque laborum magnam mollitia odio omnis perspiciatis possimus quae quod recusandae repellat similique, sit temporibus. Adipisci et facere ipsam ipsum quas!</div><div>Autem beatae consequuntur debitis delectus iure omnis! Aliquid amet aperiam autem deserunt dolores eveniet, fugiat iste nemo nihil odit officiis quam quos saepe. Consequatur eos, error quam veritatis voluptate voluptatem.</div><div>Aspernatur beatae corporis cumque distinctio dolore eaque esse fugit, iure, labore nam nemo nisi odio pariatur porro quo rem unde. Aliquid deserunt doloremque, fugit minima porro quaerat quam ratione vero.</div><div>Aliquam, consectetur, cumque cupiditate dicta ipsam ipsum iure provident quibusdam recusandae repudiandae sit unde! Ad cum delectus, ducimus fugit illo impedit incidunt perferendis quam quisquam quod repellat sed voluptas voluptatibus.</div><div>Accusamus corporis cumque dolore eius enim eos eveniet explicabo fuga impedit inventore non nostrum quo recusandae saepe, sed! Ab ea excepturi hic laudantium possimus veniam, voluptates. Blanditiis corporis dolore veniam?</div><div>Accusamus accusantium ad architecto commodi eos fugiat fugit, id incidunt ipsa ipsum iure laboriosam minus, necessitatibus neque odit perferendis praesentium quaerat quam rerum similique suscipit totam velit. Dolore, illum nostrum!</div><div>Alias dolores eius numquam saepe sed sunt! Delectus et ipsum ut. Commodi doloremque eligendi ipsam itaque, laboriosam magni quasi soluta suscipit? Distinctio eius fugiat id impedit minima, molestias optio repellat.</div><div>Aspernatur culpa cum delectus doloremque eligendi facere illo minus natus nemo, perspiciatis praesentium quam quis repellendus sapiente tempore tenetur vel velit voluptatem! At enim error, hic ipsa libero qui unde.</div><div>Aut delectus dolorum earum eum facere harum in maxime, mollitia nostrum omnis, perferendis quam quis quos repellendus tenetur totam voluptas. A ab accusantium commodi, ea enim hic optio possimus ullam?</div>" +
+            "        <p class=\"line2\">I began in 2008 going through my first half of high school in Design and Technology Academy, back then I knew I had a natural talent for programming, but followed my\n" +
+            "        family's footsteps to serve my nation after graduation. In the military I found passion in Air Traffic, teaching me to further embrace my passions and to go at everything I do with my absolute best, Air Traffic for me took nothing less, so why should I give less in anything I do? Returning in late 2021-2022\n" +
+            "        to begin programming again in Code-up LLC. Immediately following Code-up with IGA(Indie Gaming Academy) to learn how Indie gaming works and learn more C# while I was at it!</p>\n" +
             "    </div>"
     },
     {
         id: 3,
         name: 'My Skills',
         field: "    <div class=\"page page3  \"> <!--skills combine with one of the smaller paragraphs-->\n" +
-            "        ...I'm proficient in C# HTML, CSS, Javascript, Bootstrap, JQuery, and Unity so far. but I am continuing to learn\n" +
+            "        I'm proficient in C#, Java, HTML, CSS, Javascript, Bootstrap, JQuery, and Unity so far. but I am continuing to learn\n" +
             "        and am excited to expand my resources!\n" +
             "    </div>"
     },
@@ -98,7 +127,7 @@ const text = [
         id: 4,
         name: 'My Work',
         field: "  <div class=\"page page4 \"> <!--work-->\n" +
-            "        My here, I will link to you a window of my work so far growing in code up and then beyond as I improve my skills.\n" +
+            "        here, I will link to you a window of my work so far growing in code up and then beyond as I improve my skills.\n" +
             "    </div>"
     },
     {
@@ -106,13 +135,13 @@ const text = [
         name: 'Contact Me',
         field: " <div class=\"page page5\" id=\"contact text-center\"> <!--contact info-->\n" +
             "\n" +
-            "        <a href=\"#\"><img class=\"contactInfo\" src=\"assets/PNG/GitHub-Mark-Light-32px.png\" alt=\"gitHub\"></a>\n" +
+            "        <p class=\"line1\"><a href=\"#\"><img class=\"contactInfo\" src=\"assets/PNG/GitHub-Mark-Light-32px.png\" alt=\"gitHub\"><span>My Github</span></a></p>\n" +
             "        <!--Github/Manueldominguez1113-->\n" +
-            "        <a href=\"#\"><img class=\"contactInfo\" src=\"assets/ico/linkdin_iconwht.png\" alt=\"linkdin\"></a>\n" +
+            "        <p class=\"line2\"></p><a href=\"#\"><img class=\"contactInfo\" src=\"assets/ico/linkdin_iconwht.png\" alt=\"linkdin\"><span>My Linkedin</span></a></p>\n" +
             "        <!--Linkdin/Manueldominguez1113-->\n" +
-            "        <a href=\"mailto:manueldominguez1113@gmail.com?subject=Hello Manuel! I am interested to get in touch..\">\n" +
-            "                <img class=\"contactInfo\" src=\"assets/ico/emailwhtx1.png\" alt=\"Email\"></a> <!--Manueldominguez1113@gmail.com-->\n" +
-            "        <a href=\"#\">Resume</a>" +
+            "        <p class='line3'><a href=\"mailto:manueldominguez1113@gmail.com?subject=Hello Manuel! I am interested to get in touch..\">\n" +
+            "                <img class=\"contactInfo\" src=\"assets/ico/emailwhtx1.png\" alt=\"Email\"><span>My Email</span></a></p> <!--Manueldominguez1113@gmail.com-->\n" +
+            "        <a href=\"#\"></a>" +
             "    </div>"
     }
 ];
@@ -120,14 +149,12 @@ const text = [
 
 function renderText(page) {
     let inner = "";
-    windows.removeClass("animate"); // remove the animation for reset
     text.forEach((part) => {
         if (page === part.id) {
             inner = "<div id=\"textTitle\" class=\"text-center\">" + part.name + "</div>" + part.field;
         }
     })
     textArea.html(inner);
-    windows.addClass("animate"); // reset animate
 }
 
 //render page 1 so it always starts on load
@@ -152,64 +179,64 @@ contactMe.click(() => {
 
 let clouds = $("#clouds");
 //cloud generator
-setInterval(randomClouds,6000);
+setInterval(randomClouds, 6000);
 
-function randomClouds(){
-    let lR = Math.random() * (2 - 1) + 1; 
+function randomClouds() {
+    let lR = Math.random();
     let cloudNum = Math.floor(Math.random() * (5 - 1) + 1);
     let left = lR === 1;
-    
+
     clouds.html(" "); //make sure clear clouds
-    
+
     clouds.html(cloudGenerator(cloudNum, left));
-    }
+}
 
 
 function cloudGenerator(number, left) {
     switch (number) {
         case(1): {
             if (left) {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
             } else {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
             }
         }
         case(2): {
             if (left) {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday2.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight2.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
             } else {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday2.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight2.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
             }
         }
         case(3): {
-            if (leftRight === "left") {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
+            if (left) {
+                return "<img src=\"assets/portrait/clouds/cloudday3.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight3.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
             } else {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday3.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight3.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
             }
         }
         case(4): {
-            if (leftRight === "left") {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
+            if (left) {
+                return "<img src=\"assets/portrait/clouds/cloudday4.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight4.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
             } else {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday4.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight4.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
             }
         }
         case(5): {
-            if (leftRight === "left") {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
+            if (left) {
+                return "<img src=\"assets/portrait/clouds/cloudday5.png\" class=\"cloud cloud1 dayCloud passingLeft\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight5.png\" class=\"cloud cloud1 nightCloud passingLeft\" alt=\"cloud night\">";
             } else {
-                return "<img src=\"assets/portrait/cloudday.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
-                    "<img src=\"assets/portrait/cloudnight.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
+                return "<img src=\"assets/portrait/clouds/cloudday5.png\" class=\"cloud cloud1 dayCloud passingRight\" alt=\"cloud\">\n" +
+                    "<img src=\"assets/portrait/clouds/cloudnight5.png\" class=\"cloud cloud1 nightCloud passingRight\" alt=\"cloud night\">";
             }
         }
 
