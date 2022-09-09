50534d8dadc5e3c746c7281ab491cfe481ee4039
Manuel <manuel.dominguez1113@gmail.com>
2022-09-09 18:28:45 -0500

    fixed the fixing for scroll working too well adding it to every page

--- JS/mainJs.js
+++ JS/mainJs.js
@@ -95,7 +95,7 @@ const text = [
     {
         id: 1,
         name: 'Hello World!',
-        field: "<div class=\"page page1 goScroll\"> <!--home-->\n" +
+        field: "<div class=\"page page1\"> <!--home-->\n" +
             "        <p class=\"line1\">I am... <span class='pt1'>Manuel</span> <span class='pt2'>Alexander</span> <span class='pt3'>Dominguez</span></p>\n" +
             "        <p class=\"line2\">The M.A.D. Developer!</p>\n" +
             "        <p class=\"line3\">Full-Stack Dev || Game Dev || Air Traffic Controller</p>\n" +
@@ -119,7 +119,7 @@ const text = [
     {
         id: 3,
         name: 'My Skills',
-        field: "    <div class=\"page page3 goScroll\"> <!--skills combine with one of the smaller paragraphs-->\n" +
+        field: "    <div class=\"page page3\"> <!--skills combine with one of the smaller paragraphs-->\n" +
             "        I'm proficient in C#, JavaScript, Java SE, Java EE (Servlets, JSP), MySQL, Test-Driven\n" +
             "Development, Spring, Bootstrap, jQuery, Thymeleaf, HTML, CSS, and Unity so far. but I am continuing to learn\n" +
             "        and am excited to expand my resources!\n" +
@@ -135,7 +135,7 @@ const text = [
     {
         id: 5,
         name: 'Contact Me',
-        field: " <div class=\"page page5 goScroll\" id=\"contact\"> <!--contact info-->\n" +
+        field: " <div class=\"page page5\" id=\"contact\"> <!--contact info-->\n" +
             "        <p id=\"available\" class=\"announcement\">I'm avaliable for hire!</p> <p id=\"hired\" class=\"announcement\">I'm currently employed! if someone sent you here looking for a job it most likely wasn't me!</p><p id=\"freeLance\" class=\"announcement\">I'm Looking to freelance for YOU!</p><p id=\"main\" class=\"announcement\">please contact me via these links if you wish to connect with me! I look forward to hearing from you!</p>    \n" +
             "        <p class=\"line1\"><a href=\"https://github.com/Manueldominguez1113\"><img class=\"contactInfo\" src=\"assets/PNG/GitHub-Mark-Light-32px.png\" alt=\"gitHub\"><span> My Github</span></a></p>\n" +
             "        <!--Github/Manueldominguez1113-->\n" +
